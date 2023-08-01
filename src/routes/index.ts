f903258a75d1043a057998cbbc33a5be7bf15fe9
ТАЗДМБ ТАЗДМБ <fhdbudhh@gmail.com>
2023-08-01 04:19:18 +0300

    feat: added requestLogger and errorLogger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,8 +6,11 @@ import signupRoute from './signup';
 import sendNotFoundError from '../controllers/incorrectreq';
 import auth from '../middlewares/auth';
 import handleError from '../middlewares/handleError';
+import { requestLogger, errorLogger } from '../middlewares/logger';
 
 const router = Router();
+
+router.use(requestLogger);
 
 router.use('/signin', signinRoute);
 router.use('/signup', signupRoute);
@@ -15,6 +18,9 @@ router.use(auth);
 router.use('/users', usersRoutes);
 router.use('/cards', cardsRoutes);
 router.all('*', sendNotFoundError);
+
+router.use(errorLogger);
+
 router.use(handleError);
 
 export default router;
